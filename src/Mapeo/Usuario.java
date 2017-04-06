355466ccffa5773373f2ac3e5a310c9f686a0df0
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 13:15:53 -0500

    Update Usuario.java

--- src/Mapeo/Usuario.java
+++ src/Mapeo/Usuario.java
@@ -1,5 +1,98 @@
 package Mapeo;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Emilio
+ */
+@Entity
+@Table(name="usuario")
 public class Ususario{
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    	@Column(name = "usuario_id")
+    	private int usuario_id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "foto")
+	private String foto;//dirección de la foto en el servidor
+	
+	@Column(name = "correo")
+	private String correo;
+	
+	@column(nmae = "psswd")
+	private String psswd;
+	
+	@OneToMany(mappedBy = "calificaciones_usuario")
+	private List<Calificacion> calificaciones_ususario;
+	
+	@OneToMany(mappedBy = "comentarios-ususario")
+	private List<Comentario> comentarios_usuario;
+	
+	public int getUsuario_id(){
+		return ususario_id;
+	}
+	
+	public void setUsuario_id(int usuario_id){
+		this.usuario_id = ususario_id;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public void setNobre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getFoto(){
+		return foto;
+	}
+	
+	public void setFoto(String foto){
+		this.foto = foto;
+	}
+	
+	public String getCorreo(){
+		return correo;
+	}
+	
+	public void setCorreo(String correo){
+		this.correo = correo;
+	}
+	
+	public String getPsswd(){
+		return psswd;
+	}
+	
+	public void setPsswd(){
+		this.psswd = psswd;
+	}
+	
+	public List<Calificacion> getCalificaciones_usuario(){
+		return calificaciones_usuario;
+	}
+	
+	public void setCalificaciones_usuario(List<Calificacion> calificaciones_usuario){
+		this.calificaciones_usuario = calificaciones_usuario;
+	}
+	
+	public List<Comentario> getComentarios_usuario(){
+		return comentarios_usuario;
+	}
+	
+	public void setComentarios_usuario(List<Comentario> comentarios_usuario){
+		this.comentarios_usuario = comentarios_usuario;
+	}
 	
 }
