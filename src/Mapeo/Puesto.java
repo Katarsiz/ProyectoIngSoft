fbad1fcdfa513e256b2f4c4ae8b5ef05fcf0bd91
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 17:35:44 -0500

    Update Puesto.java

--- src/Mapeo/Puesto.java
+++ src/Mapeo/Puesto.java
@@ -1,5 +1,44 @@
 package Mapeo;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Emilio
+ */
+@Entity
+@Table(name="puesto")
+
 public class Puesto{
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    	@Column(name = "puesto_id")
+    	private int puesto_id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@ElementCollection
+	@CollectionTable(name = "categorias", joinColumns = @JoinColumn(name = "puesto_id"))
+	private List<String> tags;
+	
+	@Column(name = "posX")
+	private int posX;
+	
+	@Column(name = "posY")
+	private int posY;
+	
+	@OneToMany(mappedBy = "calificacion")
+	private List<Calificacion> calificaciones_ususario;
+	
+	@OneToMany(mappedBy = "comentario")
+	private List<Comentario> comentarios_usuario;
 	
 }
