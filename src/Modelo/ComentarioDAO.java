40ebf3e122d7ccfccebaab9f5677316ba39a936d
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 18:45:09 -0500

    Update ComentarioDAO.java

--- src/Modelo/ComentarioDAO.java
+++ src/Modelo/ComentarioDAO.java
@@ -1,5 +1,59 @@
 package Modelo;
 
+import Mapeo.Grupo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Emilio
+ */
 public class ComentarioDAO implements AbstractDAO{
 
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+        	this.sessionFactory = sessionFactory;
+    	}
+  	
+	@Override
+  	public void guardar(Comentario c) {
+    
+	        Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+	        try {
+			tx = session.beginTransaction();
+         
+           		session.persist(c);
+           		tx.commit();
+        	}catch (Exception e) {
+           		if (tx!=null){ 
+               			tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
+	
+	@Override
+	public void eliminar(Comentario grupo) {
+    
+        	Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+        	try {
+        	   	tx = session.beginTransaction();
+	           	session.delete(c);
+           		tx.commit();
+        	}catch (Exception e) {
+	        	if (tx!=null){ 
+        	       		tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
+  
 }
