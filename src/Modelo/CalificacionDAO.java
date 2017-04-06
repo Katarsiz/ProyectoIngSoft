eabe9330313c23a0a8a3d29ce3d9c0c251215e00
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 17:47:14 -0500

    Update CalificacionDAO.java

--- src/Modelo/CalificacionDAO.java
+++ src/Modelo/CalificacionDAO.java
@@ -1,5 +1,59 @@
 package Modelo;
 
+import Mapeo.Grupo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Emilio
+ */
 public class CalificacionDAO implements AbstractDAO{
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+        	this.sessionFactory = sessionFactory;
+    	}
+  	
+	@Override
+  	public void guardar(Celificacion c) {
+    
+	        Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+	        try {
+			tx = session.beginTransaction();
+         
+           		session.persist(c);
+           		tx.commit();
+        	}catch (Exception e) {
+           		if (tx!=null){ 
+               			tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
+	
+	@Override
+	public void eliminar(Calificacion grupo) {
+    
+        	Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+        	try {
+        	   	tx = session.beginTransaction();
+	           	session.delete(c);
+           		tx.commit();
+        	}catch (Exception e) {
+	        	if (tx!=null){ 
+        	       		tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
 
 }
