c2977114f69d7ede562f5ba94ece1b04acbc96da
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 10:59:59 -0500

    Update Calificacion.java

--- src/Mapeo/Calificacion.java
+++ src/Mapeo/Calificacion.java
@@ -1,5 +1,50 @@
 package Mapeo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+*@author Emilio
+*/
+@Entity
+@Table(name="calificacion")
 public class Calificacion{
+	
+	@ManyToOne	
+	@JoinColumn(name = "usuario_id")
+	private Ususario u;
+	
+	@ManyToOne	
+	@JoinColumn(name = "puesto_id")
+	private Puesto p;
+	
+	@Column(name = "calificacion")
+	private int estrellas;
+	
+	public Ususario getUsuario(){
+		return u;
+	}
+	
+	public void setUsuario(Usuario u){
+		this.u = u;
+	}
+	
+	public Puesto getPuesto(){
+		return p;
+	}
+	
+	public void setPuesto(Puesto p){
+		this.p = p;
+	}
+	
+	public int getCalificacion(){
+		return calificacion;
+	}
+	
+	public void setCalificacion(int calificacion){
+		this.calificacion = calificacion;
+	}
 	
 }
