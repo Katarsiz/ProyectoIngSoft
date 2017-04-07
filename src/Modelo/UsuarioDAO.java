85dc855e684eca635bbd2b1516f1aa8810d44e45
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 20:37:50 -0500

    Update UsuarioDAO.java

--- src/Modelo/UsuarioDAO.java
+++ src/Modelo/UsuarioDAO.java
@@ -1,5 +1,46 @@
 package Modelo;
 
 public class UsuarioDAO implements AbstractDAO{
+	private SessionFactory sessionFactory;
 
+	public void setSessionFactory(SessionFactory sessionFactory) {
+        	this.sessionFactory = sessionFactory;
+    	}
+  	
+  	public void guardar(Usuario c) {
+    
+	        Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+	        try {
+			tx = session.beginTransaction();
+         
+           		session.persist(c);
+           		tx.commit();
+        	}catch (Exception e) {
+           		if (tx!=null){ 
+               			tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
+	
+	public void eliminar(Usuario c) {
+    
+        	Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+        	try {
+        	   	tx = session.beginTransaction();
+	           	session.delete(c);
+           		tx.commit();
+        	}catch (Exception e) {
+	        	if (tx!=null){ 
+        	       		tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
 }
