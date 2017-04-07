0774753068cd672ee2fb3b9b968b02688c6031f3
emilio-mh <emilio-mh@ciencias.unam.mx>
2017-04-06 20:35:31 -0500

    Update PuestoDAO.java

--- src/Modelo/PuestoDAO.java
+++ src/Modelo/PuestoDAO.java
@@ -1,5 +1,46 @@
 package Modelo;
 
-public class PuestoDAO implements AbstractDAO{
+public class PuestoDAO{
+	private SessionFactory sessionFactory;
 
+	public void setSessionFactory(SessionFactory sessionFactory) {
+        	this.sessionFactory = sessionFactory;
+    	}
+  	
+  	public void guardar(Celificacion c) {
+    
+	        Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+	        try {
+			tx = session.beginTransaction();
+         
+           		session.persist(c);
+           		tx.commit();
+        	}catch (Exception e) {
+           		if (tx!=null){ 
+               			tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
+	
+	public void eliminar(Calificacion grupo) {
+    
+        	Session session = sessionFactory.openSession();
+        	Transaction tx = null;
+        	try {
+        	   	tx = session.beginTransaction();
+	           	session.delete(c);
+           		tx.commit();
+        	}catch (Exception e) {
+	        	if (tx!=null){ 
+        	       		tx.rollback();
+           		}
+           		e.printStackTrace(); 
+        	}finally {
+           		session.close();
+        	}
+	}
 }
